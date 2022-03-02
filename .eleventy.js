cfcc990d4a097522e79d74b5e12c6050da4fa75f
Kyle Geske <stungeye@gmail.com>
2022-03-02 16:58:41 -0600

    Shortcut to embed markdown inside of nunjucks files.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/mvp.css");
   eleventyConfig.addPassthroughCopy("./src/img/");
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  const md = new markdownIt({
+    html: true,
+  });
+
+  eleventyConfig.addPairedShortcode("markdown", (content) => {
+    return md.render(content);
+  });
 
   return {
     dir: {
